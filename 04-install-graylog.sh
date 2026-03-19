52437cb24a42b3e27a3a5fcfae2fd9ac4f454243
KRISSANE Alexandre <49199655+Cassandra-White@users.noreply.github.com>
2026-03-19 18:49:56 +0100

    Update 04-install-graylog.sh

--- 04-install-graylog.sh
+++ 04-install-graylog.sh
@@ -1,43 +1,40 @@
-04-install-graylog.sh
-Bash Root -- Debian
 #!/bin/bash
-# Depot Graylog 5.2
+# Depot Graylog 6.x
 wget -qO /tmp/graylog.deb \
-    https://packages.graylog2.org/repo/packages/graylog-5.2-repository_latest.deb
+    https://packages.graylog2.org/repo/packages/graylog-6.1-repository_latest.deb
 dpkg -i /tmp/graylog.deb
 apt-get update
 apt-get install -y graylog-server
 
-# Generer le password_secret (64 caracteres minimum -- obligatoire)
+# Generer le password_secret (64 caracteres minimum -- OBLIGATOIRE)
 SECRET=$(pwgen -N 1 -s 96)
 
-# Choisir le mot de passe admin puis generer son hash SHA256
-# Ici : BillU-Admin-2025! -- changer si souhaite
-MDP_ADMIN="BillU-Admin-2025!"
-HASH=$(echo -n "$MDP_ADMIN" | sha256sum | awk '{print $1}')
+# Mot de passe admin : BillU-Admin-2025!
+# Generer son hash SHA256
+MDP="BillU-Admin-2025!"
+HASH=$(echo -n "$MDP" | sha256sum | awk '{print $1}')
 
-# IP de la VM Debian
-IP=$(hostname -I | awk '{print $1}')
-
-# Editer /etc/graylog/server/server.conf
 CONF="/etc/graylog/server/server.conf"
-sed -i "s|^password_secret =.*|password_secret = $SECRET|"       $CONF
-sed -i "s|^root_password_sha2 =.*|root_password_sha2 = $HASH|"   $CONF
+
+# Injecter les valeurs dans la configuration
+sed -i "s|^password_secret =.*|password_secret = $SECRET|"           $CONF
+sed -i "s|^root_password_sha2 =.*|root_password_sha2 = $HASH|"       $CONF
 sed -i "s|#http_bind_address = .*|http_bind_address = 0.0.0.0:9000|" $CONF
-sed -i "s|#http_external_uri = .*|http_external_uri = http://$IP:9000/|" $CONF
+sed -i "s|#http_external_uri = .*|http_external_uri = http://172.16.100.21:9000/|" $CONF
 
 echo ""
-echo "============================================="
+echo "==========================================="
 echo " Graylog configure"
-echo "  Interface web : http://$IP:9000"
-echo "  Login         : admin"
-echo "  Mot de passe  : $MDP_ADMIN"
-echo "============================================="
+echo "  Interface : http://172.16.100.21:9000"
+echo "  Login     : admin"
+echo "  Mot passe : $MDP"
+echo "==========================================="
 
 # Activer et demarrer
 systemctl daemon-reload
 systemctl enable graylog-server
 systemctl start graylog-server
 
-echo "Demarrage Graylog en cours (60 a 90 secondes)..."
-echo "Verifier avec : systemctl status graylog-server"
+echo ""
+echo "Demarrage en cours -- attendre 90 secondes"
+echo "Puis ouvrir http://172.16.100.21:9000 dans un navigateur"
